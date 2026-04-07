1afc57bc8671d7c85dfe20c7195bfdbbdd08e84b
Rezwan Ahmed Ratul <rezwanahmedratul007@gmail.com>
2026-04-07 23:47:58 +0600

    add problem statement and solution template for "A. The Equalizer"

---
+++ A_The_Equalizer.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pll> vpl;
+typedef map<int,int> mii;
+
+#define fastInput() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define el "\n"
+#define pb push_back
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define allr(a) a.rbegin(), a.rend()
+#define all(a) a.begin(), a.end()
+#define vin(v) for(auto &u : v) cin >> u
+#define vout(v) for(auto u : v) cout << u << " "
+#define F first
+#define S second
+#define sp ' '
+#define mem(a,b) memset(a,b,sizeof(a))
+#define gcd(a,b) __gcd(a,b)
+int const MOD = 1e9 + 7;
+
+void solve(){
+    
+}
+
+int main(){
+    fastInput();
+    
+    ll t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
