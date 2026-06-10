b8539aec2e11ab07550a866c5b5de7223f55f209
Rezwan Ahmed Ratul <rezwanahmedratul007@gmail.com>
2026-06-10 17:32:19 +0600

    add solutions for Codeforces Round 1102 (Div. 2): A. Euclid, Sequence and Two Numbers and B. Palindrome, Twelve and Two Terms

---
+++ A_Euclid_Sequence_and_Two_Numbers.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pll> vpl;
+typedef map<int,int> mii;
+
+#define fastInput() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define el "\n"
+#define pb push_back
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define allr(a) a.rbegin(), a.rend()
+#define all(a) a.begin(), a.end()
+#define vin(v) for(auto &u : v) cin >> u
+#define vout(v) for(auto u : v) cout << u << " "
+#define F first
+#define S second
+#define sp ' '
+#define mem(a,b) memset(a,b,sizeof(a))
+#define gcd(a,b) __gcd(a,b)
+int const MOD = 1e9 + 7;
+
+void solve(){
+    int n;
+    cin >> n;
+    vl a(n);
+    vin(a);
+    sort(allr(a));
+    if(n==2){
+        vout(a);
+        cout << el;
+    } else{
+        bool flag = true;
+        for(int i =2; i<n;i++){
+            if(!(a[i] == a[i-2]%a[i-1])){
+                flag = false;
+                break;
+            }
+        }
+        if(flag){
+            cout << a[0] << sp << a[1] << el;
+        } else {
+            cout << "-1\n";
+        }
+    }
+}
+
+int main(){
+    fastInput();
+    
+    ll t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
---
+++ B_Palindrome_Twelve_and_Two_Terms.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pll> vpl;
+typedef map<int,int> mii;
+
+#define fastInput() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define el "\n"
+#define pb push_back
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+#define vin(v) for(auto &u : v) cin >> u
+#define vout(v) for(ll i = 0; i < (ll)v.size(); i++) cout << v[i] << (i + 1 == (ll)v.size() ? '\n' : ' ')
+#define F first
+#define S second
+#define sp ' '
+#define mem(a,b) memset(a,b,sizeof(a))
+#define gcd(a,b) __gcd(a,b)
+const int MOD = 1e9 + 7;
+
+void solve(){
+    
+}
+
+int main(){
+    fastInput();
+
+    ll t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
